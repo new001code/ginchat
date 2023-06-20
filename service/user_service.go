62cd7dc4bae978a007dfbb3a341b189b0cff2d00
new001 <lhl_creeper@163.com>
2023-06-20 20:25:54 +0800

    log util

--- service/user_service.go
+++ service/user_service.go
@@ -4,6 +4,7 @@ import (
 	"ginchat/models"
 	query "ginchat/models/query"
 	"ginchat/sql"
+	"ginchat/util"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -16,9 +17,13 @@ var userDao = &sql.UserDao{}
 
 var ApiResponse = &models.ApiResponse{}
 
+// log
+var Log = &util.Log{}
+
 func (u *UserService) Register(c *gin.Context) {
 	var user query.UserQuery
 	if err := c.ShouldBind(&user); err == nil {
+		Log.Debugf("user register param: %+v\n", user)
 		err = userDao.Create(&user)
 		if err == nil {
 			c.JSON(http.StatusOK, ApiResponse.SuccessDefault())
@@ -26,11 +31,17 @@ func (u *UserService) Register(c *gin.Context) {
 			c.JSON(http.StatusOK, ApiResponse.FailWithMessage(err.Error()))
 		}
 	} else {
-
+		Log.Errorln("参数解析错误：", err)
 	}
 
 }
 
 func (u *UserService) Login(c *gin.Context) {
 	var user query.UserQuery
+	if err := c.ShouldBind(&user); err == nil {
+		Log.Debugf("user login param: %+v\n", user)
+
+	} else {
+		Log.Errorln("参数解析错误：", err)
+	}
 }
